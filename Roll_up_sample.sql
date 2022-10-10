421d9bdb75dce12c898ae168fe4454255b88081a
Bamidele Adeola <ajamubamidele@gmail.com>
2022-10-10 22:27:18 +0100

    Rollup Sample

--- Roll_up_sample.sql
+++ Roll_up_sample.sql
@@ -1,3 +1,4 @@
+SELECT * FROM fortune500;
 
 SELECT 
 	sector, 
@@ -11,3 +12,40 @@ SELECT
 	SUM(revenues) AS total_revenue
 FROM fortune500
 GROUP BY ROLLUP (sector); --Group revenue with a mindset of total revenue for all sectors
+
+
+-- ROLL UP WITH 2 GROUPING SETS
+SELECT
+    sector,
+    industry,
+    SUM(revenues) AS total_revenue
+FROM
+    fortune500
+GROUP BY
+    ROLLUP(sector, industry);
+
+/*
+Note that if you change the order of a grouping sets industry, sector 
+the result will be different as shown in the following query:
+*/
+
+SELECT
+    sector,
+    industry,
+    SUM(revenues) AS total_revenue
+FROM
+    fortune500
+GROUP BY
+    ROLLUP(industry, sector);
+
+
+-- A partial rollup is also possible as seen below:
+SELECT
+    sector,
+    industry,
+    SUM(revenues) AS total_revenue
+FROM
+    fortune500
+GROUP BY 
+	sector,
+    ROLLUP(industry);
