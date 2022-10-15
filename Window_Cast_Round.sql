72c00f6bf22f6739e12d75ad0c2bab070d7fb7fe
Bamidele Adeola <ajamubamidele@gmail.com>
2022-10-15 21:56:13 +0100

    Cast_Round_WindowFunc

---
+++ Window_Cast_Round.sql
@@ -0,0 +1,7 @@
+--Using window function of OVER() and PARTITION BY Clause
+SELECT title As Company, revenues, sector,
+	SUM(revenues) OVER (PARTITION BY sector) AS rev_totals,
+	ROUND(AVG(revenues::numeric) OVER (PARTITION by sector), 2) AS avgRev,
+	MAX(revenues) OVER (pARTITION by sector) AS maxRev,
+	MIN(revenues) OVER (pARTITION by sector) AS minRev
+FROM fortune500;
