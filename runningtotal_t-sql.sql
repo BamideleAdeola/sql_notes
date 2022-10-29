0315fb135cab6f4ef2de9d1149d7349e2142c7b8
Bamidele Adeola <ajamubamidele@gmail.com>
2022-10-29 10:13:22 +0100

    running total updates and images

--- runningtotal_t-sql.sql
+++ runningtotal_t-sql.sql
@@ -3,6 +3,15 @@ SELECT
 	id,
 	Country,
 	Points,
-	SUM(Points) OVER (ORDER BY id ASC) AS RunningTotal  -- cummulative addition of points
+	SUM(Points) OVER (ORDER BY id ASC) AS RunningTotal  -- cumulative addition of points
 FROM eurovision;
 
+-- Also, you can use it for orders;
+SELECT
+order_id,
+sales_date,
+salesperson,
+order_value,
+SUM(order_value)
+  OVER (ORDER BY order_id ASC) AS RunningTotal
+FROM sales_history;
